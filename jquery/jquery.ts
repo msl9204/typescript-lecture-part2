bc6b3b3c5e63c3f3c40d071ef95cec33a623f4d7
minsoo.lee <letomok@gmail.com>
2022-09-17 10:29:54 +0900

    esModuleInterop

--- jquery/jquery.ts
+++ jquery/jquery.ts
@@ -10,3 +10,18 @@ $(tag).html(function (i: number) {
     console.log(this);
     return $(this).data('name') + '입니다';
 });
+
+/**
+ * typescript에서 cjs 모듈 사용법
+ *
+ * import $ = require('jquery')
+ * export = $
+ *
+ * import * as $ from 'jquery'
+ * 이렇게 하면 다되는 형태 (cjs + es6)
+ *
+ * tsconfig.json에서
+ * esModuleInterop: true 를 하면
+ * cjs 모듈도 import React from 'react' 형태로 사용 가능
+ *
+ */
