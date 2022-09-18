a03e62570caf61a7450a2b461b8323b597d08a8a
minsoo.lee <letomok@gmail.com>
2022-09-18 21:22:58 +0900

    axios Generic

--- axios/t_axios.ts
+++ axios/t_axios.ts
@@ -1,9 +1,39 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
+
+type Post = {userId: number, id: number, title: string, body: string}
+type Created = {
+
+}
+type Data = { title: string, body: string, userId: number }
+
+
 
 (async () => {
     try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        const response = await axios.get<Post>('https://jsonplaceholder.typicode.com/todos/1')
         console.log(response.data)
+        console.log(response.data.userId)
+        console.log(response.data.body)
+        console.log(response.data.id)
+        console.log(response.data.title)
+
+        const response2 = await axios.post<Created, AxiosResponse<Created>, Data>('https://jsonplaceholder.typicode.com/posts', {
+            title: 'foo',
+            body: 'bar',
+            userId: 1
+        })
+
+        const response3 = await axios({
+            method: 'post',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            data: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }
+        })
+
+
     } catch (error) {
 
     }
