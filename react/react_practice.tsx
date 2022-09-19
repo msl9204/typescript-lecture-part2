70b2a3daa3c7ac32c154d759fa851dedd2d7f40f
minsoo.lee <letomok@gmail.com>
2022-09-19 22:18:10 +0900

    react FC/VFC

---
+++ react/react_practice.tsx
@@ -0,0 +1,66 @@
+import React, {useState, useCallback, useRef, useEffect, FunctionComponent, FC, VFC, ReactNode} from 'react'
+
+interface P {
+    name: string
+    title: string
+    children?: ReactNode | undefined
+}
+
+const WordRelay: FC<P> = (props) => {
+    const [word, setWord] = useState("제로초");
+    const [value, setValue] = useState("");
+    const [result, setResult] = useState("");
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        console.log("useEffect")
+    }, [])
+
+    const onSubmitForm = useCallback((e) => {
+        e.preventDefault()
+        const input = inputEl.current
+        if (word[word.length - 1] === value[0]) {
+            setResult("딩동댕");
+            setWord(value);
+            setValue('');
+            if (input) {
+                input.focus()
+            }
+        } else {
+            setResult("땡");
+            setValue('');
+            if (input) {
+                input.focus()
+            }
+        }
+    }, [word, value]);
+
+    const onChange = useCallback((e) => {
+        setValue(e.currentTarget.value)
+    }, [])
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    ref={inputEl}
+                    value={value}
+                    onChange={onChange}
+                />
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
+}
+
+const Parent = () => {
+    return (
+        <WordRelay name="네임" title="타이틀">
+            <div></div>
+        </WordRelay>
+    )
+}
+
+export default WordRelay;
