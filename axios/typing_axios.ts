211962302b2bc7e714b7518dddc6267ec4b318f4
minsoo.lee <letomok@gmail.com>
2022-09-19 21:43:52 +0900

    axios 타이핑

--- axios/typing_axios.ts
+++ axios/typing_axios.ts
@@ -5,23 +5,43 @@ type Created = {
 
 }
 type Data = { title: string, body: string, userId: number }
+interface Config<D = any> {
+    method?: 'post' | 'get' | 'put' | 'patch' | 'delete' | 'head' | 'options'
+    url?: string,
+    data?: D
+
+}
 
 (async () => {
+
+    /**
+     * axios 타이핑
+     */
+    interface A {
+        get<T = any, R = AxiosResponse<T>>(url: string): Promise<R>
+        post<T = any, R = AxiosResponse<T>, D = any>(url: string, data: D): Promise<R>
+        (config: Config): void
+        (url: string, config: Config): void
+        isAxiosError(error: unknown): error is AxiosError
+    }
+
+    const a: A = axios
+
     try {
-        const response = await axios.get<Post>('https://jsonplaceholder.typicode.com/todos/1')
+        const response = await a.get<Post>('https://jsonplaceholder.typicode.com/todos/1')
         console.log(response.data)
         console.log(response.data.userId)
         console.log(response.data.body)
         console.log(response.data.id)
         console.log(response.data.title)
 
-        const response2 = await axios.post<Created, AxiosResponse<Created>, Data>('https://jsonplaceholder.typicode.com/posts', {
+        const response2 = await a.post<Created, AxiosResponse<Created>, Data>('https://jsonplaceholder.typicode.com/posts', {
             title: 'foo',
             body: 'bar',
             userId: 1
         })
 
-        const response3 = await axios({
+        const response3 = await a({
             method: 'post',
             url: 'https://jsonplaceholder.typicode.com/posts',
             data: {
@@ -31,9 +51,18 @@ type Data = { title: string, body: string, userId: number }
             }
         })
 
+        const response4 = await a('https://jsonplaceholder.typicode.com/posts', {
+            method: 'post',
+            data: {
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }
+        })
+
 
     } catch (error) {
-        if (axios.isAxiosError(error)) {
+        if (a.isAxiosError(error)) {
             console.error((error.response as AxiosResponse<{message: string}>)?.data.message)
         }
         // if (error instanceof AxiosError) { // 커스텀 타입가드
@@ -43,17 +72,9 @@ type Data = { title: string, body: string, userId: number }
         // let axiosError = (error as AxiosError).response
         // console.error(axiosError?.data)
 
+
+
+
     }
 
 })()
-
-/**
- * 함수안에 속성들을 넣을 수 있다.
- */
-const a = () => {}
-a.create = () => {}
-a.isAxiosError = () => {}
-a.z = '123'
-
-a()
-a.create()
