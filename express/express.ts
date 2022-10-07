7aac30b21cb153b914b26ec83ee3f9b0d5a766cd
minsoo.lee <letomok@gmail.com>
2022-10-07 21:03:14 +0900

    express middleware 타이핑

--- express/express.ts
+++ express/express.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {NextFunction} from 'express';
 
 const app = express()
 
@@ -8,6 +8,21 @@ app.use(express.urlencoded({extended: false}))
 app.get('/', (req, res) => {
 
 })
+
+// 합쳐서 쓸 수 있다.
+interface Response extends Express.Response {
+    name: string
+}
+
+const middleware = (req: Express.Request, res: Response, next: NextFunction) => {
+    res.name
+}
+
+// 미들웨어는 RequestHandler 타입이다.
+app.use((err, req, res, next) => {
+    console.log(err.staus)
+})
+
 
 app.listen(8080, () => {
 
